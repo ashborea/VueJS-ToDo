fb33dcb83fe978d70d391cf631ccb2567b36f6cd
Ash Borea <ashborea@gmail.com>
2024-02-10 14:21:59 +0100

    creating header component & todo input

---
+++ src/views/ToDoView.vue
@@ -0,0 +1,27 @@
+<script setup>
+import ToDoCreator from '@/components/ToDoCreator.vue';
+
+
+</script>
+
+<template>
+  <main>
+    <h1>Create ToDo</h1>
+    <ToDoCreator />
+  </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 40px 16px;
+
+  h1 {
+    margin-bottom: 16px;
+    text-align: center;
+  }
+}</style>
